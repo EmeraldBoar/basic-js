547777053f6a6f8b7e3127e57acbfed667cdeb41
EmeraldBoar <kozlovkv174@gmail.com>
2021-09-14 22:14:11 +0300

    fix: change the variable name 'result' to 'age'

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -25,7 +25,7 @@ export default function dateSample(sampleActivity) {
   const age = Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD));
 
   if (age > 0 && age !== Infinity) {
-    return result;
+    return age;
   }
   return false;
 }
